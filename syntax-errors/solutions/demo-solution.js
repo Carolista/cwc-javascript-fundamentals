fd80f35d589c0262a610718b0f232b8450f39195
Carolista <cjones.renaissancewoman@gmail.com>
2026-04-03 03:10:00 -0400

    added syntax-errors demo and practice

--- syntax-errors/solutions/demo-solution.js
+++ syntax-errors/solutions/demo-solution.js
@@ -1,17 +1,16 @@
-/**** DEBUGGING SYNTAX ERRORS IN JAVASCRIPT ****/
+/**** DEBUGGING SYNTAX ERRORS: DEMO SOLUTION ****/
 
 /*
     Syntax errors happen to everyone, even the most experienced of developers.
     Getting familiar with how to spot them will help you debug quickly and move on. 
-    Here's some practice with the most common ones!
+    Here's some examples of the most common ones!
 */
 
-// Delete lines 16 and 32, then run the code below from the terminal and 
-// fix the errors one-by-one, using the error message JavaScript provides to 
-// help you locate them. 
+// Run the code below from the terminal and fix the errors one-by-one, 
+// using the error messages JavaScript provides to help you locate them. 
 
 // NOTE: It doesn't always point to the exact point where the error occurs, so 
-// you'll need to look at the greater context.
+// you may need to look at the greater context.
 
 console.log('JavaScript'); // mismatched quotes
 
@@ -29,4 +28,4 @@ console.log(Boolean("Strings with lengths greater than 0 are 'truthy!'"));
 // missing double quote AND missing parenthesis
 
 // All fixed? Great job! Now you are better equipped to fix some of the 
-// simplest errors that inevitably pop up.
+// simplest errors that will inevitably pop up in the future.
---
+++ syntax-errors/solutions/practice-solution.js
@@ -0,0 +1,39 @@
+const input = require('readline-sync');
+
+/**** DEBUGGING SYNTAX ERRORS: PRACTICE SOLUTION ****/
+
+// TODO: Delete the two lines indicated below, then run the code below from the terminal and
+// fix the errors one-by-one, using the error messages JavaScript provides to locate them.
+
+// NOTE: This is tricky. Pay close attention to the details on each line of code and use
+// what you know about proper syntax to identify issues. Sometimes the error messages JavaScript 
+// will give you are misleading (it does its best, but it doesn't always see the big picture). 
+// Also, some errors may not be caught until runtime because of the conditional paths.
+
+let eggsResponse = input.question(
+	'\nGood morning. I was thinking about making breakfast. Do we have any eggs? Y / N ',
+);
+let hasEggs = eggsResponse.toUpperCase() === 'Y'; // true or false
+
+if (hasEggs) {
+	let cheeseResponse = input.question( // Fixed typo in method name.
+		'\nGreat! What about cheese? Do we have any cheddar? Y / N ',
+	);
+	let hasCheese = cheeseResponse.toUpperCase() === 'Y'; // true or false
+
+	if (hasCheese) { // Was a string instead of variable reference
+		console.log("\nPerfect! I'll make us some omelettes.");
+	} else {
+		console.log(
+			"\nToo bad. I was in the mood for cheesy omelettes. I'll make some scrambled eggs instead.",
+            // Fixed quotes here. I chose to use double quotes to enclose, but you may have chosen
+            // to escape the apostrophe instead. Either works!
+		);
+	}
+} else {
+    console.log("\nI guess I'll put those on the grocery list. Cereal it is!");
+} // Was missing the closing brace
+
+// Make sure you tested all possible logical paths since there are nested conditional blocks 
+// involved. You'll know you're done when all three console.log statements have printed 
+// successfully (depending on the path). 
