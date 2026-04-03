25d77355838ff3425e58939c9efd65aa0182a61d
Carolista <cjones.renaissancewoman@gmail.com>
2026-04-03 03:48:53 -0400

    added practice with demo for printing to the console

---
+++ printing-to-the-console/exercises/practice.js
@@ -0,0 +1,26 @@
+/**** PRINTING TO THE CONSOLE: PRACTICE ****/
+
+/*
+    Printing to the console is easy, but worth practicing so you build
+    some "muscle memory." It is absolutely essential for coding. Here's 
+    an opportunity for you to get some hands-on practice.
+*/
+
+// TODO: Write console.log statements for each of the following pieces of information. 
+// Test each one by running the file in the terminal as you go.
+// Remember that strings must be in single or double quotes (e.g.: "JavaScript"). You'll
+// learn more about data types like strings and numbers very soon.
+
+// 1. Print the name of one of your all-time favorite music artists or bands.
+
+// 2. Print your favorite pizza toppings.
+
+// 3. Print the name of one of your pets (or the pet you wish you had).
+
+// 4. Print your favorite season of the year (winter, spring, summer, or fall).
+
+// 5. Print a whole sentence describing the reason you're learning to code.
+
+// 6. Print your preference: sweet or salty snacks?
+
+// Keep going! Practice printing as many things as you want to get comfortable with it.
--- printing-to-the-console/solutions/demo-solution.js
+++ printing-to-the-console/solutions/demo-solution.js
@@ -1,22 +1,22 @@
-/**** PRINTING TO THE CONSOLE ****/
+/**** PRINTING TO THE CONSOLE: DEMO SOLUTION ****/
 
 /*
     console.log is a special method built into JavaScript that allows us to
-    produce output in the console. When you run the file from the terminal/CLI
-    with Node, any output will appear below your command.
+    send output to the console. When you run the file from the terminal/CLI, 
+    any output will appear below your node command.
 */
 
 // NOTE: Open this file on your local machine in VS Code to edit it. To run it, you need to
 // open a terminal (either the integrated terminal in VS Code or a separate application)
 // and navigate to the directory where this file resides.
 
-// TODO: In the terminal, navigate to the directory where this file resides and
+// In the terminal, navigate to the directory where this file resides and
 // run it with the node command to see the result of the code on the next line
 console.log('JavaScript');
 
-// TODO: On the next line, write the code that will print your own name to the console,
-// then run the file again from the terminal. Make sure to enclose your name with 
-// either single quotes or double quotes. Example: "Carrie" or 'Carrie'
+// On the next line, write the code that will print your own name to the console,
+// then run the file again from the terminal. You should see both logs appear.
+console.log('Carrie');
 
 // That's all there is to it! Now you'll be able to test your code and see its output
 // in future exercises.
