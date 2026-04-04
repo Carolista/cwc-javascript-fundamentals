98769b15cab1c79038657794b5df5308d4935f30
Carolista <cjones.renaissancewoman@gmail.com>
2026-04-04 03:34:58 -0400

    added declaring-variables exercises

---
+++ declaring-variables/solutions/practice-solution.js
@@ -0,0 +1,45 @@
+/**** DECLARING AND INITIALIZING VARIABLES: PRACTICE SOLUTION ****/
+
+/*
+    Time to practice declaring and initializing variables! 
+    
+    Remember:
+        - Always use the 'let' keyword when declaring
+        - Don't use the 'let' keyword if initializing later on a separate line.
+        - Give your variable a meaningful name using the camelCase convention.
+        - Use the assignment operator = to initialize with a value.
+        - Use single or double quotes for strings (collections of characters).
+        - Do not use quotes for numbers like 24, 3.7, or -85.
+*/
+
+// 1. Create a variable called myFavoriteMovie that stores the name
+// of one of your favorite movies, assign a value, then print the
+// variable to the console to see its stored value.
+let myFavoriteMovie = "House of Flying Daggers";
+console.log(myFavoriteMovie);
+
+// 2. Create a variable called futureVacationSpot that stores a location
+// you plan to visit one day, assign a value, then print the variable to the
+// console to see its stored value.
+let futureVacationSpot = "Iceland";
+console.log(futureVacationSpot);
+
+// 3. Create a variable called perfectTemp that stores your ideal temperature
+// outside on a 'perfect day', assign a value, then print the variable
+// to the console to see its stored value.
+let perfectTemp = 72;
+console.log(perfectTemp);
+// Also okay if you initialized to a string like "72°F"
+
+// 4A. Declare a variable called numberOfStatesVisited, but do not initialize it yet.
+let numberOfStatesVisited;
+
+// 4B. Think about how many states in the U.S. you've been to. For some of us
+// this could be a pretty big number that requires a lot of thought! Here's a tool you can use:
+// https://www.fla-shop.com/visited-states/?vc=1ca032&uc=90cfea&hc=40bfa6&bc=ffffff&ss=on&sl=on
+// Once you have the correct total, write another line of code below to assign it to the variable you
+// created above. Then print the variable to the console to see its stored value.
+numberOfStatesVisited = 32;
+console.log(numberOfStatesVisited);
+
+// Great job! You will get LOTS more practice with this in future exercises.
