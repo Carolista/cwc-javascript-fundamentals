09d2dfd4cdb0437568c0e4c7d9aade262288845a
Carolista <cjones.renaissancewoman@gmail.com>
2026-05-03 09:13:34 -0400

    fixed basic boolean expressions demo so that variables are attached to specific examples

--- tutorials/02-conditionals/basic-boolean-expressions/exercises/demo.js
+++ tutorials/02-conditionals/basic-boolean-expressions/exercises/demo.js
@@ -1,27 +1,30 @@
-/**** BASIC BOOLEAN EXPRESSIONS: SOLUTION ****/
+/**** BASIC BOOLEAN EXPRESSIONS ****/
+
+/** EXAMPLE 1 - STRICT EQUALITY **/
 
 let hatchCode = "1234-Alpha";
 let inputCode = "1234-Alpha";
-let shuttleVelocityMPH = 20000;
-let satelliteVelocityKPH = "15000";
-const KILOMETERS_PER_MILE = 1.60934;
-let currentSector = "Orion";
-let currentOxygen = 15;
-let requiredOxygen = 20;
-
-/** EXAMPLE 1 - STRICT EQUALITY **/
 
 // DEMO Check to see if the input code matches the hatch code.
 
 /** EXAMPLE 2 - LOOSE EQUALITY **/
 
+let shuttleVelocityMPH = 9500;
+let satelliteVelocityKPH = "15000";
+const KILOMETERS_PER_MILE = 1.60934;
+
 // DEMO: Check whether the shuttle has matched the satellite velocity yet
 
 /** EXAMPLE 3 - INEQUALITY **/
 
+let currentSector = "Orion";
+
 // DEMO: Check if the current sector is NOT our destination.
 
 /** EXAMPLE 4 - GREATER THAN & LESS THAN **/
+
+let currentOxygen = 15;
+let requiredOxygen = 20;
 
 // DEMO: Check if we have enough oxygen to survive.
 
